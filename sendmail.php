c0ee76073401d253cfbdcb3a10c551b3dbb5fe16
Andrei Bolvakh <119782822+minskCityEugeny@users.noreply.github.com>
2023-03-11 20:57:26 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,42 +1,17 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->IsHTML(rtue);
-
-$mail->setForm('kondratenkozh@yandex.by');
-$mail->addAddress('kondratenkozhhh@gmail.com');
-$mail->Subject = 'Заявка';
-
-$body = '<h1>Заявка на обработку<h1>';
-
-if(term(!empty($_POST['name']))){
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'<p>';
-}
-if(term(!empty($_POST['email']))){
-    $body.='<p><strong>Email</strong> '.$_POST['email'].'<p>';
-}
-if(term(!empty($_POST['phone']))){
-    $body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'<p>';
-}
-if(term(!empty($_POST['message']))){
-    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'<p>';
-}
-
-if (!$mail->send()) {
-    $message = 'Ошибка';
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
 } else {
-    $message = 'Данные отправлены!'
-}
-
-$response = ['message' => $message];
-
-header('Content-type: application/json');
-echo json_encode($response);
-?>
+    echo "при отправке сообщения возникли ошибки";
+}?>
